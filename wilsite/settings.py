468774d33af60ac03ff6b5ffef421a267cbdcb04
Pinkfloyd <pinkfloyd.adonay@gmail.com>
2020-02-17 16:17:56 +0800

    Hello

--- wilsite/settings.py
+++ wilsite/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'wilsite.wsgi.application'
 
 # for emailing apps
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default='True', cast=bool)
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.mail.yahoo.com')
-EMAIL_HOST_USER = config('EMAIL_HOST_NAME', default='wildcatlabs@yahoo.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD', default='wildcatlabs101!')
-EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_HOST_USER = config('EMAIL_HOST_NAME', default='wildcatinnolabs@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD', default='wildcatinnolabs101!')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 
 
 # Database
