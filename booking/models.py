bd4493cf269d530de4b92b901c274e3ba8c5ff3d
Jay Vince Serato <jdserato@up.edu.ph>
2020-03-04 12:25:58 +0800

    Updated functionality

--- booking/models.py
+++ booking/models.py
@@ -1,23 +1,29 @@
 from django.db import models
 from account.models import Client
+from django.core.validators import MaxValueValidator, MinValueValidator
 PURPOSES = (
 	('Studying', 'Studying'),
 	('Project Making', 'Project Making'),
 	('Group Collaboration', 'Group Collaboration'),
 )
 VENUES = (
-	('conf', 'Conference Room'),
-	('conf2', 'Joined Conference Room'),
-	('coworking', 'Coworking Space'),
+	('Conference Room A', 'Conference Room A'),
+	('Conference Room B', 'Conference Room B'),
+	('Joined Conference Room', 'Joined Conference Room'),
+	('Coworking Space', 'Coworking Space'),
 )
 
 class Venue(models.Model):
+	min_name = models.CharField(max_length=20, default="none")
 	name = models.CharField(max_length=50)
 	cap = models.IntegerField()
+	has_computers = models.BooleanField(default=False)
+	computers = models.IntegerField(default=0)
 	isSimultaneous = models.BooleanField(default=False)
+	cost = models.IntegerField(default=20)
 
 class Booking(models.Model):
-	title = models.CharField(max_length=100)
+	title = models.CharField(max_length=100, blank=True)
 	startDate = models.DateField()
 	endDate = models.DateField()
 	startTime = models.TimeField()
@@ -25,5 +31,6 @@ class Booking(models.Model):
 	purpose = models.CharField(max_length=30, choices=PURPOSES, default='Studying')
 	attendee = models.CharField(max_length=30, default='none')
 	referenceNo = models.IntegerField()
-	cost = models.IntegerField()
-	venue = models.CharField(max_length=30, choices=VENUES, default='coworking')
+	computers = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(8)])
+	cost = models.IntegerField(blank=True)
+	venue = models.CharField(max_length=30, choices=VENUES, default='Coworking Space')
